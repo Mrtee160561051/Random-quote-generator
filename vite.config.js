8ad93dd2e9a2453b3c2ae89be2e5285b83421e40
BiggyTech <akinyemitestimonyiadedayo@gmail.com>
2024-07-13 20:55:17 +0530

    Change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Random-quote-App/",
+  base:"/Random-quote-generator/",
   plugins: [react()],
 })
